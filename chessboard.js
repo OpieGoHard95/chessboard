379292219767aa75938aa785d029e93e6631757e
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-01-24 21:47:25 -0500

    Added var size for the length

--- chessboard.js
+++ chessboard.js
@@ -1,6 +1,7 @@
 var chess1 = " # # # #"
 var chess2 = "# # # # "
-for (var linenumber = 1; linenumber <= 8; linenumber++) {
+var size = 8
+for (var linenumber = 1; linenumber <= size; linenumber++) {
   if (linenumber % 2 == 0)
       console.log(chess2);
   else
